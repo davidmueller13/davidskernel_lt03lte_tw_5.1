1afddcb4e36becb797d1cdc5a227e768a78dcb72
davidmueller13 <david.mueller1013@gmail.com>
2016-11-06 12:57:15 +0100

    REVERT:fix compiling (reverted from commit fd69a26409f76c2847eec0783c331252ab7783fe)

--- security/sdp/pub_crypto_emul.c
+++ security/sdp/pub_crypto_emul.c
@@ -481,7 +481,7 @@ void pub_crypto_control_init(pub_crypto_control_t *con) {
 }
 
 static int __init pub_crypto_mod_init(void) {
-#if (LINUX_VERSION_CODE > KERNEL_VERSION(3,4,113))
+#if (LINUX_VERSION_CODE > KERNEL_VERSION(3,4,0))
 	struct netlink_kernel_cfg cfg = {
 		.input  = crypto_recver,
 	};
